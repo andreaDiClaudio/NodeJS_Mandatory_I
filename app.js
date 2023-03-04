7c2eebfe7ce75883c414340ca789f9830be51dfa
andreaDiClaudio <andreadicla@gmail.com>
2023-03-04 17:21:31 +0100

    Created home page

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/landingPage/landingPage.html")
 });
 
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/public/home/home.html")
+});
+
+app.post("/", (req, res) => {
+    res.redirect("/home")
+});
+
 const PORT = 8080;
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log("Server is running on port: ", PORT);
