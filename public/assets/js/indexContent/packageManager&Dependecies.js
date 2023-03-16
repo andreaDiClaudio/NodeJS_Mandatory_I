b2a25b47be4489e8f9a7be623f1902d7f618307d
andreaDiClaudio <andreadicla@gmail.com>
2023-03-16 13:34:05 +0100

    Finishes the package manager topic. Started the Express topic

---
+++ public/assets/js/indexContent/packageManager&Dependecies.js
@@ -0,0 +1,30 @@
+"use strict";
+
+console.log("Hello");
+
+const indexContent1 = document.querySelector(".index-content");
+
+//a tags
+const a1 = document.createElement("a");
+a1.href = "#1";
+a1.className = "index-topic";
+a1.innerText = "1. Maven"
+indexContent1.appendChild(a1);
+
+const a2 = document.createElement("a");
+a2.href = "#2";
+a2.className = "index-topic";
+a2.innerText = "2. npm";
+indexContent1.appendChild(a2);
+
+const a3 = document.createElement("a");
+a3.href = "#3";
+a3.className = "index-topic";
+a3.innerText = "3. Maven vs. npm";
+indexContent1.appendChild(a3);
+
+const a4 = document.createElement("a");
+a4.href = "#4";
+a4.className = "index-topic";
+a4.innerText = "4. How to install packages";
+indexContent1.appendChild(a4);
