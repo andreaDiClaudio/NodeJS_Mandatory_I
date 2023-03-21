d7f0f7a8ce02e20bec8e6595ffd8bdb7968c4472
andreaDiClaudio <andreadicla@gmail.com>
2023-03-21 19:08:13 +0100

    Finishes about js topic

---
+++ public/assets/js/indexContent/aboutJs.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const indexContent1 = document.querySelector(".index-content");
+
+//a tags
+const a1 = document.createElement("a");
+a1.href = "#1";
+a1.className = "index-topic";
+a1.innerText = "1. Javacript"
+indexContent1.appendChild(a1);
+
+const a2 = document.createElement("a");
+a2.href = "#2";
+a2.className = "index-topic";
+a2.innerText = "2. Variable and Data Types";
+indexContent1.appendChild(a2);
+
+const a3 = document.createElement("a");
+a3.href = "#3";
+a3.className = "index-topic";
+a3.innerText = "3. Type Coercion";
+indexContent1.appendChild(a3);
+
+const a4 = document.createElement("a");
+a4.href = "#4";
+a4.className = "index-topic";
+a4.innerText = "4. Loose vs. Strict Equality Operator";
+indexContent1.appendChild(a4);
+
+const a5 = document.createElement("a");
+a5.href = "#5";
+a5.className = "index-topic";
+a5.innerText = "5. Strict Mode";
+indexContent1.appendChild(a5);
+
+const a6 = document.createElement("a");
+a6.href = "#6";
+a6.className = "index-topic";
+a6.innerText = "6. Function"
+indexContent1.appendChild(a6);
+
+const a7 = document.createElement("a");
+a7.href = "#7";
+a7.className = "index-topic";
+a7.innerText = "7. Callback Function";
+indexContent1.appendChild(a7);
+
+const a8 = document.createElement("a");
+a8.href = "#8";
+a8.className = "index-topic";
+a8.innerText = "8. Loops";
+indexContent1.appendChild(a8);
+
+const a9 = document.createElement("a");
+a9.href = "#9";
+a9.className = "index-topic";
+a9.innerText = "9. Date and Time";
+indexContent1.appendChild(a9);
+
+const a10 = document.createElement("a");
+a10.href = "#10";
+a10.className = "index-topic";
+a10.innerText = "10. fetch()";
+indexContent1.appendChild(a10);
